63795a1e4d1e61802715a235e896a97561ef02c5
unknown <wiktor.dabrowa98@gmail.com>
2023-04-23 13:38:20 +0200

    --logs logs 100 files

--- skrypt.sh
+++ skrypt.sh
@@ -6,6 +6,13 @@ do
         --date)
             date
             ;;
+        --logs)
+            for i in {1..100}
+            do
+                touch "log$i.txt"
+                echo "log$i" >> "log$i.txt"
+                echo "skrypt.sh" >> "log$i.txt"
+                date >> "log$i.txt" 
+            done
     esac 
 done
-echo 'outside'
