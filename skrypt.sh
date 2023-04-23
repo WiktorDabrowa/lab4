f619e49592f7a5ee056e2f5c349f7d43df3f92eb
unknown <wiktor.dabrowa98@gmail.com>
2023-04-23 15:04:46 +0200

    final

--- skrypt.sh
+++ skrypt.sh
@@ -49,14 +49,70 @@ for (( i=1; i<=$#; i++ )); do
             echo ' --logs | -l : ';
             echo '      - with integer argument: creates provided amount of log files';
             echo '      - without argument: creates 100 log files'
-            echo ' --help | -h: this command'
+            echo ' --help | -h: this command';
+            echo ' --init | -i: clone repository to working directory';
+            echo ' --error | -e: --init + creates specified amount of errorx files';
 
             ;;
         --init | -i)
+            if ! [ -d './repo' ] 
+                then
+                    mkdir 'repo';
+            fi;
             cd repo;
-            echo $PWD;
             repo_url="https://github.com/WiktorDabrowa/lab3";
             git clone "$repo_url" "$PWD";
+            export PATH="$PWD:$PATH";
+            ;;
+        --error | -e)
+
+            if ! [ -d './repo' ] 
+            then
+                mkdir 'repo';
+            fi
+            cd repo;
+            repo_url="https://github.com/WiktorDabrowa/lab3";
+            git clone "$repo_url" "$PWD";
+            export PATH="$PWD:$PATH";
+
+            next_arg=$((i+1))
+            # check if next argument exist:
+            if (( $next_arg <= $# ))
+                then
+                    # check if next argument is number:
+                    if [[ "${!next_arg}" =~ ^[0-9]+$ ]];
+                        then
+                            # if it is a number run flag 
+                            # with additional arguments
+                            for (( i=1; i<=${!next_arg}; i++))
+                            do
+                                touch "error$i.txt"
+                                echo "error$i" >> "error$i.txt"
+                                echo "skrypt.sh" >> "error$i.txt"
+                                date >> "error$i.txt"
+                            done
+                        else
+                        # else run loop 100 times:
+                            for i in {1..100}
+                            do
+                                touch "error$i.txt"
+                                echo "error$i" >> "error$i.txt"
+                                echo "skrypt.sh" >> "error$i.txt"
+                                date >> "error$i.txt" 
+                            done
+                    fi
+                else
+                    # else run loop 100 times:
+                    for i in {1..100}
+                    do
+                        touch "error$i.txt"
+                        echo "error$i" >> "error$i.txt"
+                        echo "skrypt.sh" >> "error$i.txt"
+                        date >> "error$i.txt" 
+                    done
+            fi
+
+
 
     esac 
 done
