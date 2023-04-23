e6bdaff363474b57a0aa0371ff7439ccaaab4238
unknown <wiktor.dabrowa98@gmail.com>
2023-04-23 14:26:23 +0200

    --logs flag accepts integer arguments

--- skrypt.sh
+++ skrypt.sh
@@ -1,18 +1,48 @@
 #!/bin/bash
 
-for var in "$@"
-do
-    case $var in
+for (( i=1; i<=$#; i++ )); do
+    case "${!i}" in
         --date)
             date
             ;;
         --logs)
-            for i in {1..100}
-            do
-                touch "log$i.txt"
-                echo "log$i" >> "log$i.txt"
-                echo "skrypt.sh" >> "log$i.txt"
-                date >> "log$i.txt" 
-            done
+            next_arg=$((i+1))
+            # check if next argument exist:
+            if (( $next_arg <= $# ))
+                then
+                    # check if next argument is number:
+                    if [[ "${!next_arg}" =~ ^[0-9]+$ ]];
+                        then
+                            # if it is a number run flag 
+                            # with additional arguments
+                            for (( i=1; i<=${!next_arg}; i++))
+                            do
+                                touch "log$i.txt"
+                                echo "log$i" >> "log$i.txt"
+                                echo "skrypt.sh" >> "log$i.txt"
+                                date >> "log$i.txt"
+                            done
+                        else
+                        # else run loop 100 times:
+                            for i in {1..100}
+                            do
+                                touch "log$i.txt"
+                                echo "log$i" >> "log$i.txt"
+                                echo "skrypt.sh" >> "log$i.txt"
+                                date >> "log$i.txt" 
+                            done
+                    fi
+                else
+                    # else run loop 100 times:
+                    for i in {1..100}
+                    do
+                        touch "log$i.txt"
+                        echo "log$i" >> "log$i.txt"
+                        echo "skrypt.sh" >> "log$i.txt"
+                        date >> "log$i.txt" 
+                    done
+            fi
+            
+            ;;
     esac 
 done
